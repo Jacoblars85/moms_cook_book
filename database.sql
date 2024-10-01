88738ef831cb5e14aefd594fbbe75a9359ad1e36
Jacob Larson <hockeybobber@comcast.net>
2024-09-30 19:51:26 -0500

    updated db

--- database.sql
+++ database.sql
@@ -3,7 +3,8 @@ DROP TABLE IF EXISTS "user";
 
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (16) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "email" VARCHAR (100) UNIQUE NOT NULL,
+    "name" VARCHAR(200),
+    "password" VARCHAR (100) NOT NULL
 );
 	  
