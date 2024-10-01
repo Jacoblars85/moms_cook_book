66b0fe0462e61297ae9b6f07e7bd0cb49ebf090f
Jacob Larson <hockeybobber@comcast.net>
2024-09-30 19:51:46 -0500

    added new error messagaes for logging in and registering

--- src/redux/reducers/errors.reducer.js
+++ src/redux/reducers/errors.reducer.js
@@ -7,11 +7,13 @@ const loginMessage = (state = '', action) => {
     case 'CLEAR_LOGIN_ERROR':
       return '';
     case 'LOGIN_INPUT_ERROR':
-      return 'Enter your username and password!';
+      return 'Enter your email and password!';
     case 'LOGIN_FAILED':
-      return "Oops! The username and password didn't match. Try again!";
+      return "Oops! The email and password didn't match. Try again!";
     case 'LOGIN_FAILED_NO_CODE':
       return 'Oops! Something went wrong! Is the server running?';
+    case 'LOGIN_FAILED_PASSWORDS_DONT_MATCH':
+      return `Oops! The email and password didn't match. Try again!`;
     default:
       return state;
   }
@@ -24,9 +26,13 @@ const registrationMessage = (state = '', action) => {
     case 'CLEAR_REGISTRATION_ERROR':
       return '';
     case 'REGISTRATION_INPUT_ERROR':
-      return 'Choose a username and password!';
+      return 'Choose a email and password!';
     case 'REGISTRATION_FAILED':
-      return "Oops! That didn't work. The username might already be taken. Try again!";
+      return "Oops! That didn't work. Enter valid email, name and company!";
+    case 'REGISTRATION_FAILED_PASSWORDS_DONT_MATCH':
+      return `Oops! The passwords didn't match. Try again!`;
+    case 'REGISTRATION_FAILED_DUPLICATE_EMAIL':
+      return 'Oops! The email is already taken. Try again!';
     default:
       return state;
   }
