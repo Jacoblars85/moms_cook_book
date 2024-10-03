1ac84a9db1ac2ec8af2651c2ec3d3181fc5a7060
Jacob Larson <hockeybobber@comcast.net>
2024-10-02 20:34:44 -0500

    removed old button

--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -8,20 +8,13 @@ function RegisterPage() {
 
   return (
     <div>
+      <center>
       <h1>Mom's Cookbook</h1>
 
       <RegisterForm />
 
-      <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={() => {
-            history.push("/login");
-          }}
-        >
-          Login
-        </button>
+      
+
       </center>
     </div>
   );
