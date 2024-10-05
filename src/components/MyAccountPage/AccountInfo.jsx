9ccaf0443f6488630d371b5fd7d111c60fdbbc52
Jacob Larson <hockeybobber@comcast.net>
2024-10-04 21:01:40 -0500

    commented out code that doesnt work yet

--- src/components/MyAccountPage/AccountInfo.jsx
+++ src/components/MyAccountPage/AccountInfo.jsx
@@ -18,17 +18,17 @@ import {
             <TableBody>
               <TableRow>
                 <TableCell>Name:</TableCell>
-                <TableCell>{currentUser.name}</TableCell>
+                {/* <TableCell>{currentUser.name}</TableCell> */}
               </TableRow>
   
               <TableRow>
                 <TableCell>Email:</TableCell>
-                <TableCell>{profileUser.email}</TableCell>
+                {/* <TableCell>{profileUser.email}</TableCell> */}
               </TableRow>
   
               <TableRow>
                 <TableCell>Company:</TableCell>
-                <TableCell>{profileUser.company_name}</TableCell>
+                {/* <TableCell>{profileUser.company_name}</TableCell> */}
               </TableRow>
             </TableBody>
           </Table>
